264e6754633877744e21fb665ea6f48be3583926
DeivideMateus <44448709+DeivideMateus@users.noreply.github.com>
2020-05-29 22:50:38 -0300

    Armengo Bot Busca

--- untitled/run.py
+++ untitled/run.py
@@ -4,9 +4,6 @@ from telegram.ext import Updater
 from telegram.ext import CommandHandler, MessageHandler, Filters, RegexHandler
 from untitled.Busca import searchPath
 import logging
-import networkx as nx
-
-
 
 updater = Updater(token='1101037221:AAHYw9MDlyLCD_njw_ZsZTZHMjp1Wceu3h8', use_context=True)
 dispatcher = updater.dispatcher
@@ -17,8 +14,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 def start(update, context):
     context.bot.send_chat_action(chat_id=update.message.chat_id, action=ChatAction.TYPING)
 
-    msg = "Olá eu sou o *Data2LearningBot*, vamos conversar?" \
-          "\nEnvie uma imagem para que eu possa classifica-la. "
+    msg = "Olá, eu sou o Bot IA. Digite o comando desejado:\n /busca - Retorna o caminho e custo entre 2 pontos"
 
     context.bot.send_message(chat_id=update.message.chat_id, text=msg, parse_mode=ParseMode.MARKDOWN)
 
@@ -26,53 +22,69 @@ def start(update, context):
 def busca(update, context):
     context.bot.send_chat_action(chat_id=update.message.chat_id, action=ChatAction.TYPING)
 
-    msg = searchPath('A', 'Q')
+    msg = 'Digite 2 pontos entre A e U em maiúsculo e separados por espaço, para descobrir o menor caminho e o custo entre eles. ' \
+          '\n Ex: A Q'
 
     context.bot.send_message(chat_id=update.message.chat_id, text=msg, parse_mode=ParseMode.MARKDOWN)
 
 
-
-def echo(update, context):
-    context.bot.send_chat_action(chat_id=update.message.chat_id, action=ChatAction.TYPING)
-
-    reply_keyboard = [['Sim', 'Não']]
-
-    text = "No momento só reconheço imagens. Você deseja enviar uma imagem para que eu possa classifica-la?"
-
-
-    update.message.reply_text(text, reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
-
-def unknown(update, context):
-    context.bot.send_message(chat_id=update.message.chat_id, text="Desculpa, não entendi o comando digitado =/")
-
-
-def register_new(update, context):
+def caminho(update, context):
     context.bot.send_chat_action(chat_id=update.message.chat_id, action=ChatAction.TYPING)
 
     text_answer = update.message.text
+    list = text_answer.split()
+    v1 = list[0]
+    v2 = list[1]
 
-    if text_answer == "Sim":
+    msg = searchPath(v1, v2)
 
-        update.message.reply_text('É só anexar uma imagem :)',
-                                  reply_markup=ReplyKeyboardRemove())
-    else:
-        update.message.reply_text('Tudo bem, mas quando quiser que eu reconheça uma imagem é só anexar.',
-                                  reply_markup=ReplyKeyboardRemove())
+    context.bot.send_message(chat_id=update.message.chat_id, text=msg, parse_mode=ParseMode.MARKDOWN)
 
 
 start_handler = CommandHandler('start', start)
-
 busca_handler = CommandHandler('busca', busca)
-registernew_handler = MessageHandler(Filters.regex('(Sim|Não)$'), register_new)
-echo_handler = MessageHandler(Filters.text, echo)
-unknown_handler = MessageHandler(Filters.command, unknown)
+caminho_handler = MessageHandler(Filters.text, caminho)
 
-dispatcher.add_handler(busca_handler)
+
 dispatcher.add_handler(start_handler)
-dispatcher.add_handler(registernew_handler)
-dispatcher.add_handler(echo_handler)
-dispatcher.add_handler(unknown_handler)
+dispatcher.add_handler(busca_handler)
+dispatcher.add_handler(caminho_handler)
 
 updater.start_polling()
 
 updater.idle()
+
+# def echo(update, context):
+#    context.bot.send_chat_action(chat_id=update.message.chat_id, action=ChatAction.TYPING)
+#
+# reply_keyboard = [['Sim', 'Não']]
+
+# text = "No momento só reconheço imagens. Você deseja enviar uma imagem para que eu possa classifica-la?"
+
+
+# update.message.reply_text(text, reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+
+# def unknown(update, context):
+# context.bot.send_message(chat_id=update.message.chat_id, text="Desculpa, não entendi o comando digitado =/")
+
+
+# def register_new(update, context):
+# context.bot.send_chat_action(chat_id=update.message.chat_id, action=ChatAction.TYPING)
+
+# text_answer = update.message.text
+
+# if text_answer == "Sim":
+#
+#   update.message.reply_text('É só anexar uma imagem :)',
+#                             reply_markup=ReplyKeyboardRemove())
+# else:
+#   update.message.reply_text('Tudo bem, mas quando quiser que eu reconheça uma imagem é só anexar.',
+#                             reply_markup=ReplyKeyboardRemove())
+
+# registernew_handler = MessageHandler(Filters.regex('(Sim|Não)$'), register_new)
+# echo_handler = MessageHandler(Filters.text, echo)
+# unknown_handler = MessageHandler(Filters.command, unknown)
+
+# dispatcher.add_handler(registernew_handler)
+# dispatcher.add_handler(echo_handler)
+# dispatcher.add_handler(unknown_handler)
