25044cf1c1dd35930d20fe7d1360e1dfba0c3ec4
DeivideMateus <44448709+DeivideMateus@users.noreply.github.com>
2020-06-11 15:43:15 -0300

    recomendaçao chat bot

--- untitled/BotRecomendacao/BotRecomendacao.py
+++ untitled/BotRecomendacao/BotRecomendacao.py
@@ -1,8 +1,11 @@
+from imdbpie import Imdb
 from telegram import ParseMode, ChatAction
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
-from untitled.BotBusca.Busca import searchPath
 import logging
+from IPython.display import Image, display
+
+from untitled.BotRecomendacao.Recomendacao import get_imdb_id, get_movie_title, recomendacao
 
 updater = Updater(token='1101037221:AAHYw9MDlyLCD_njw_ZsZTZHMjp1Wceu3h8', use_context=True)
 dispatcher = updater.dispatcher
@@ -14,27 +17,30 @@ def start(update, context):
     context.bot.send_chat_action(chat_id=update.message.chat_id, action=ChatAction.TYPING)
 
     msg = "Olá, eu sou o Bot IA. Digite o comando desejado:\n" \
-          "1 - /busca P1 P2: Retorna o caminho e custo entre 2 pontos de A até U.\n" \
-          "Ex: /busca C T (vai retornar o caminho e custo entre o ponto C e T)"
+          "1 - /recomendacao id: Retorna uma lista de filmes recomendados para um determinado usuário de 1 a 20.\n" \
+          "Ex: /recomendacao 1 (vai retornar uma lista de filmes recomendados para o usuário 1)"
 
     context.bot.send_message(chat_id=update.message.chat_id, text=msg, parse_mode=ParseMode.MARKDOWN)
 
 
-def busca(update, context):
+def filmes(update, context):
     context.bot.send_chat_action(chat_id=update.message.chat_id, action=ChatAction.TYPING)
 
-    v1 = context.args[0].upper()
-    v2 = context.args[1].upper()
-    msg = searchPath(v1, v2)
+    user = context.args[0]
+
+    recomendacao(user)
+
+    msg = user
+
 
     context.bot.send_message(chat_id=update.message.chat_id, text=msg, parse_mode=ParseMode.MARKDOWN)
 
 
 start_handler = CommandHandler('start', start)
-busca_handler = CommandHandler('busca', busca, pass_args=True)
+filmes_handler = CommandHandler('filmes', filmes, pass_args=True)
 
 dispatcher.add_handler(start_handler)
-dispatcher.add_handler(busca_handler)
+dispatcher.add_handler(filmes_handler)
 
 updater.start_polling()
 
---
+++ untitled/BotRecomendacao/Recomendacao.py
@@ -0,0 +1,109 @@
+import pandas as pd
+
+
+idx = pd.IndexSlice
+links = pd.read_csv("links.csv", index_col=['movieId'])
+movies = pd.read_csv("movies.csv", sep=",", index_col=['movieId'])
+ratings = pd.read_csv("ratings.csv", index_col=['userId', 'movieId'])
+tags = pd.read_csv("tags.csv", index_col=['userId', 'movieId'])
+
+
+def get_movies_by_user(id_user, rating_cut=0, list_=False):
+    return_dict = {}
+    dict_ = ratings.loc[idx[id_user, :], 'rating'].T.to_dict()
+
+    for d in dict_:
+        if rating_cut != 0:
+            if dict_[d] >= rating_cut:
+                return_dict[d[1]] = dict_[d]
+        else:
+            return_dict[d[1]] = dict_[d]
+
+    if list_:
+        return list(return_dict.keys())
+
+    return return_dict
+
+
+def get_users_by_movie(id_movie, rating_cut=0, list_=False):
+    return_dict = {}
+    dict_ = ratings.loc[idx[:, id_movie], 'rating'].T.to_dict()
+    for d in dict_:
+        if rating_cut != 0:
+            if dict_[d] >= rating_cut:
+                return_dict[d[0]] = dict_[d]
+        else:
+            return_dict[d[0]] = dict_[d]
+
+    if list_:
+        return list(return_dict.keys())
+
+    return return_dict
+
+
+def get_rating_by_user_movie(id_user, id_movie):
+    rating = 0.0;
+
+    try:
+        rating = ratings.loc[idx[id_user, id_movie], 'rating']
+    except KeyError as e:
+        rating = 0.0
+
+    return rating
+
+
+def get_all_users():
+    return list(set([x[0] for x in ratings.index.values]))
+
+
+def get_movie_title(id_movie):
+    info = movies.loc[idx[id_movie], :]
+    return info['title']
+
+
+def get_imdb_id(id_movie):
+    imdbid = int(links.loc[idx[id_movie], 'imdbId'])
+
+    imdbid = "tt%.7d" % imdbid
+
+    return imdbid
+
+
+all_users = get_all_users()
+
+movies_user_true = {}
+movies_user_false = {}
+
+for user in all_users:
+    movies_user_true[user] = get_movies_by_user(user, list_=True)
+    movies_user_false[user] = get_movies_by_user(user, list_=False)
+
+
+def recomendacao(user):
+
+    selected_user = user
+
+    my_movies = get_movies_by_user(selected_user, rating_cut=4, list_=True)
+
+    all_users = []
+
+    for movie in my_movies:
+        all_users = all_users + get_users_by_movie(movie, rating_cut=5, list_=True)
+
+        all_users = list(set(all_users))
+
+        all_movies = []
+
+    for user in all_users:
+        movies_ = get_movies_by_user(user, rating_cut=5, list_=True)
+        all_movies = all_movies + movies_
+
+    all_movies = list(set(all_movies) - set(my_movies))
+
+    return all_movies
+
+
+
+
+
+
